3edd4c039163920f3b63d742a2b0a2abc56014e6
Mikhail Nuñez <mnunez@exist.com>
2020-07-23 13:48:31 +0800

    Updates

---
+++ precache-manifest.db0abc25556738fb89959ca17ac5c06d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "19d45dc3456c5c87a6c48b67942aa8f1",
+    "url": "/ygo-db/index.html"
+  },
+  {
+    "revision": "ed7dd4ec83774c239489",
+    "url": "/ygo-db/static/css/main.5facb584.chunk.css"
+  },
+  {
+    "revision": "9f1e7b54c847cb9242aa",
+    "url": "/ygo-db/static/js/2.a0d31d8b.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/ygo-db/static/js/2.a0d31d8b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ed7dd4ec83774c239489",
+    "url": "/ygo-db/static/js/main.29821170.chunk.js"
+  },
+  {
+    "revision": "6cd0f440560da06f039b",
+    "url": "/ygo-db/static/js/runtime-main.f38e1e84.js"
+  }
+]);
